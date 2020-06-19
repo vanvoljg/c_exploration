abbf528b3018d0b70bc3786c95dd6bd9fcb54d8f
Jesse Van Volkinburg <vanvoljg@gmail.com>
2020-06-19 13:48:25 -0700

    updated precision and tests
    Use long double internally to get maximum precision, then cast to double on return. Test runner updated.

--- src/nderiv.c
+++ src/nderiv.c
@@ -1,24 +1,63 @@
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <tgmath.h>
 
 #define HLIMIT 10e-6
+#define PI     3.1415926535
 
-double long nderiv(double x);
+double ndersin(double x);
+int tcmp(double testVal, double realVal);
+int test(double x, size_t count);
+void test_suite(void);
 
-double long
-nderiv(double x)
+// Numerical derivative of sin(x)
+double
+ndersin(double x)
 {
-  return 0;
-};
+  const long double lx = (long double)x;
+  const long double dx = 2 * HLIMIT;
+  const long double dy = sinl(lx + HLIMIT) - sinl(lx - HLIMIT);
+  const long double fx = dy / dx;
+  return (double)fx;
+}
+
+int
+tcmp(double val, double real)
+{
+  return (fabs(val - real) < HLIMIT);
+}
 
 int
 main(void)
 {
-  const double t1x = 0.5;
-  const double long t1in = sin(t1x);
-  const double long t1out = cos(t1x);
-
+  test_suite();
   return EXIT_SUCCESS;
-};
+}
+
+void test_suite(void) {
+  size_t count, pass, fail = 0;
+  puts("Begin test suite\n");
+  // Test case 1, x = 0;
+  count++;
+  test(0, count) ? pass++ : fail++;
+  // Test case 2, x = pi / 2;
+  count++;
+  test(PI / 2, count) ? pass++ : fail++;
+  puts("Test suite complete");
+  printf("Pass: %lx\nFail: %lx\n", pass, fail);
+}
+
+int
+test(double x, size_t count)
+{
+  const double tinp = sin(x);
+  const double tans = cos(x);
+  const double tres = ndersin(x);
+  const int pass = tcmp(tres, tans);
+  printf("  Test: %lx\t\tx=\t%f\n", count, x);
+  printf("\t\tf(x)=sin(x)=\t%f\n", tinp);
+  printf("\t\tf'(x)=cos(x)=\t%f\n", tans);
+  printf("\t\tnderiv(x)=\t%f\n", tres);
+  printf("  Test %lx Pass: %s\n\n", count, pass ? "PASS" : "FAIL");
+  return pass; 
+}
